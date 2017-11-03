09067f59fad3edad766d20e000ac86fa196138da
kletsseeokay <32551324+kletsseeokay@users.noreply.github.com>
2017-11-03 16:31:57 -0400

    This update actually works
    Run this (preferably at a raw MySQL prompt) to create the nessecary database tables.  
    NOTE: When a dependency is updated, the update cascades, but if the dependency's deleted, then the field value is set to null.

--- config/Table_Layout.sql
+++ config/Table_Layout.sql
@@ -1,53 +1,90 @@
+CREATE DATABASE cs3500FinalProj;
+
+USE cs3500FinalProj;
+
 CREATE TABLE user (
-	uid		INT PRIMARY KEY AUTO_INCREMENT,
+	uid		INT AUTO_INCREMENT,
 	username	VARCHAR(50) NOT NULL,
-	password	VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE ticket (
-	tid		INT PRIMARY KEY AUTO_INCREMENT,
-	uid		INT FOREIGN KEY REFERENCES user(uid),
-	tstate		INT FOREIGN KEY REFERENCES stateMapping(tstate),
-	ptype		INT FOREIGN KEY REFERENCES problemMapping(pid),
-	userDesc	TEXT,
-	userTitle	TINYTEXT
-);
-
-CREATE TABLE stateMapping (
-	tstate		INT PRIMARY KEY,
-	stateName	VARCHAR(50)
-);
-
-CREATE TABLE problemMapping (
-	pid		INT PRIMARY KEY AUTO_INCREMENT,
-	probName	VARCHAR(20)
-);
-
-CREATE TABLE solutionFilter (
-	solfid		INT PRIMARY KEY AUTO_INCREMENT,
-	OS		INT FOREIGN KEY REFERENCES OSMapping(osid),
-	browser		INT FOREIGN KEY REFERENCES browserMapping(browserid),
-	problem		INT FOREIGN KEY REFERENCES problemMapping(pid),
-	tag		INT FOREIGN KEY REFERENCES tagMapping(tagid),
-	suggid		INT FOREIGN KEY REFERENCES suggMapping(suggid)
+	password	VARCHAR(50) NOT NULL,
+	PRIMARY KEY (uid)
 );
 
 CREATE TABLE OSMapping (
-	osid		INT PRIMARY KEY AUTO_INCREMENT,
-	osName		VARCHAR(50)
+	osid		INT AUTO_INCREMENT,
+	osName		VARCHAR(50),
+	PRIMARY KEY (osid)
 );
 
 CREATE TABLE browserMapping (
-	browserid	INT PRIMARY KEY AUTO_INCREMENT,
-	browName	VARCHAR(100)
+	browserid	INT AUTO_INCREMENT,
+	browName	VARCHAR(100),
+	PRIMARY KEY (browserid)
 );
 
 CREATE TABLE tagMapping (
-	tagid		INT PRIMARY KEY AUTO_INCREMENT,
-	tag		VARCHAR(100)
+	tagid		INT AUTO_INCREMENT,
+	tag		VARCHAR(100),
+	PRIMARY KEY (tagid)
 );
 
 CREATE TABLE suggMapping (
-	suggid		INT PRIMARY KEY AUTO_INCREMENT,
-	suggText	VARCHAR(100)
+	suggid		INT AUTO_INCREMENT,
+	suggText	VARCHAR(100),
+	PRIMARY KEY (suggid)
+);
+
+CREATE TABLE stateMapping (
+	tstate		INT AUTO_INCREMENT,
+	stateName	VARCHAR(50),
+	PRIMARY KEY (tstate)
+);
+
+CREATE TABLE problemMapping (
+	pid		INT AUTO_INCREMENT,
+	probName	VARCHAR(20),
+	PRIMARY KEY (pid)
+);
+
+CREATE TABLE ticket (
+	tid		INT AUTO_INCREMENT,
+	uid		INT,
+	tstate		INT,
+	ptype		INT,
+	userDesc	TEXT,
+	userTitle	TINYTEXT,
+	PRIMARY KEY (tid),
+	FOREIGN KEY (uid) REFERENCES user(uid)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE,
+	FOREIGN KEY (tstate) REFERENCES stateMapping(tstate)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE,
+	FOREIGN KEY (ptype) REFERENCES problemMapping(pid)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE
+);
+
+CREATE TABLE solutionFilter (
+	solfid		INT AUTO_INCREMENT,
+	OS		INT,
+	browser		INT,
+	problem		INT,
+	tag		INT,
+	suggid		INT,
+	PRIMARY KEY (solfid),
+	FOREIGN KEY (OS) REFERENCES OSMapping(osid)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE,
+	FOREIGN KEY (browser) REFERENCES browserMapping(browserid)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE,
+	FOREIGN KEY (problem) REFERENCES problemMapping(pid)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE,
+	FOREIGN KEY (tag) REFERENCES tagMapping(tagid)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE,
+	FOREIGN KEY (suggid) REFERENCES suggMapping(suggid)
+		ON DELETE SET NULL
+		ON UPDATE CASCADE
 );
