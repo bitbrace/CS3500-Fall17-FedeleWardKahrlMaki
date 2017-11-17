3c365e1d51600ffb7f314ff50473b97261378fb9
KristinaMaki5172 <kristinamaki987@gmail.com>
2017-11-17 16:12:37 -0500

    Basic Header and Footer files
    Have the main outline for the header and footer files. We can add
    colors and other elements as needed.

---
+++ CoreFiles/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <p>Stuff above - (get rid of this)</p>
+    <div class="container">
+        <div style='border-top:1px solid #ccc;'></div>
+        <div>
+            <p><small><i>&copy Graet Help 2017</i></small></p>
+        </div>
+    </div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</footer>
---
+++ CoreFiles/header.php
@@ -0,0 +1,29 @@
+<header>
+     <div class="container">
+         <nav class="navbar navbar-inverse " role="navigation">
+             <div class="navbar-header">
+                <h1 class="navbar-text"><strong>Graet Help</strong></h1>
+            </div>
+           
+
+            <div>
+               <ul class="nav">
+                    <li class="nav-item"><a href="Dashboard.php" class="nav-link" >Dashboard</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link" >About</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link" >Contact Us</a></li>
+                   <?php
+                        if (isset($_GET['uid'])){
+                            echo '<li class="nav-item"><a href="logout.php" class="nav-link">Log Out</a></li>';
+                        }
+                        else{
+                            echo '<li class="nav-item"><a href="login.php" class="nav-link">Log In</a></li>';
+                        }
+                   ?>
+                   
+                </ul>
+            </div>
+         </nav>
+          <div style='border-bottom:1px solid #ccc;'></div>
+      </div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</header>
