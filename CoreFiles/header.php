3b63607cb15d25dc45384923781b09e0d33da28d
KristinaMaki5172 <kristinamaki987@gmail.com>
2017-11-19 23:01:24 -0500

    Put everything in the header and footer as echoes in php functions.

--- CoreFiles/header.php
+++ CoreFiles/header.php
@@ -1,4 +1,6 @@
-<header>
+<?php
+function header(){
+echo '<header>
      <div class="container">
          <nav class="navbar navbar-inverse " role="navigation">
              <div class="navbar-header">
@@ -10,20 +12,22 @@
                <ul class="nav">
                     <li class="nav-item"><a href="Dashboard.php" class="nav-link" >Dashboard</a></li>
                     <li class="nav-item"><a href="#" class="nav-link" >About</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link" >Contact Us</a></li>
-                   <?php
+                    <li class="nav-item"><a href="#" class="nav-link" >Contact Us</a></li>';
+                   
                         if (isset($_GET['uid'])){
                             echo '<li class="nav-item"><a href="logout.php" class="nav-link">Log Out</a></li>';
                         }
                         else{
                             echo '<li class="nav-item"><a href="login.php" class="nav-link">Log In</a></li>';
                         }
-                   ?>
                    
-                </ul>
+                   
+                echo '</ul>
             </div>
          </nav>
-          <div style='border-bottom:1px solid #ccc;'></div>
+          <div style="border-bottom:1px solid #ccc;"></div>
       </div>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-</header>
+</header>';
+}
+?>
