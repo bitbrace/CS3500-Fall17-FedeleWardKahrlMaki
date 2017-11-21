115cf18e13f27ab8e625a42712c8f6172e379863
KristinaMaki5172 <kristinamaki987@gmail.com>
2017-11-21 12:47:56 -0500

    Changed to post

--- CoreFiles/header.php
+++ CoreFiles/header.php
@@ -1,5 +1,5 @@
 <?php
-function header(){
+
 echo '<header>
      <div class="container">
          <nav class="navbar navbar-inverse " role="navigation">
@@ -14,7 +14,7 @@ echo '<header>
                     <li class="nav-item"><a href="#" class="nav-link" >About</a></li>
                     <li class="nav-item"><a href="#" class="nav-link" >Contact Us</a></li>';
                    
-                        if (isset($_GET['uid'])){
+                        if (isset($_POST['uid'])){
                             echo '<li class="nav-item"><a href="logout.php" class="nav-link">Log Out</a></li>';
                         }
                         else{
@@ -29,5 +29,5 @@ echo '<header>
       </div>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </header>';
-}
+
 ?>
