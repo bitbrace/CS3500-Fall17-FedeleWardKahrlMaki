16fd330a77290cc00bfa77cc2fbd49899f5c39e8
KristinaMaki5172 <kristinamaki987@gmail.com>
2017-11-21 12:57:25 -0500

    Changed a couple things

--- CoreFiles/footer.php
+++ CoreFiles/footer.php
@@ -1,13 +1,13 @@
 <?php
 
-echo '<footer>
+<footer>
     <div class="container">
-        <div style="border-top:1px solid #ccc;"></div>
+        <div style='border-top:1px solid #ccc;'></div>
         <div>
-            <p><small><i>&copy Graet Help 2017</i></small></p>
+            echo '<p><small><i>&copy Graet Help 2017</i></small></p>';
         </div>
     </div>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-</footer>';
+</footer>
 
 ?>
--- CoreFiles/header.php
+++ CoreFiles/header.php
@@ -1,16 +1,16 @@
 <?php
 
-echo '<header>
+<header>
      <div class="container">
          <nav class="navbar navbar-inverse " role="navigation">
              <div class="navbar-header">
-                <h1 class="navbar-text"><strong>Graet Help</strong></h1>
+                echo '<h1 class="navbar-text"><strong>Graet Help</strong></h1>';
             </div>
            
 
             <div>
                <ul class="nav">
-                    <li class="nav-item"><a href="Dashboard.php" class="nav-link" >Dashboard</a></li>
+                    echo '<li class="nav-item"><a href="Dashboard.php" class="nav-link" >Dashboard</a></li>
                     <li class="nav-item"><a href="#" class="nav-link" >About</a></li>
                     <li class="nav-item"><a href="#" class="nav-link" >Contact Us</a></li>';
                    
@@ -22,12 +22,12 @@ echo '<header>
                         }
                    
                    
-                echo '</ul>
+                </ul>
             </div>
          </nav>
-          <div style="border-bottom:1px solid #ccc;"></div>
+          <div style='border-bottom:1px solid #ccc;'></div>
       </div>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-</header>';
+</header>
 
 ?>
