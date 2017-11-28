60dd542fd438f2d5c8e29d888c7db618fe44d474
KristinaMaki5172 <kristinamaki987@gmail.com>
2017-11-28 18:45:44 -0500

    Post

--- Resources/submitTicketUpdate.php
+++ Resources/submitTicketUpdate.php
@@ -1,13 +1,13 @@
 <?php 
 
 // User updated
-if (isset($_GET['subCreate'])){
+if (isset($_POST['subCreate'])){
 
 	$stmt = $database->prepare("INSERT INTO ticket (uid, tstate, ptype, userDesc) VALUES (:user, 1, :problem, :description);");
 
-	$stmt->bindParam(':user', $_GET['uid'], PDO::PARAM_INT);
-	$stmt->bindParam(':problem', $_GET['ptype'], PDO::PARAM_INT);
-	$stmt->bindParam(':description', $_GET['userDesc'], PDO::PARAM_STR);
+	$stmt->bindParam(':user', $_POST['uid'], PDO::PARAM_INT);
+	$stmt->bindParam(':problem', $_POST['ptype'], PDO::PARAM_INT);
+	$stmt->bindParam(':description', $_POST['userDesc'], PDO::PARAM_STR);
 
 	if ($stmt->execute() === TRUE){
 	echo("<div class='well'>
@@ -20,23 +20,23 @@ if (isset($_GET['subCreate'])){
 	}
 }
 
-if (isset($_GET['subUpdate'])){
+if (isset($_POST['subUpdate'])){
 
 
 	$stmt = $database->prepare("UPDATE ticket SET ptype = :problem, userDesc = :description, tstate = :state WHERE tid = :ticket;");
 
-	$stmt->bindParam(':problem', $_GET['ptype'], PDO::PARAM_INT);
-	$stmt->bindParam(':description', $_GET['userDesc'], PDO::PARAM_STR);
-	$stmt->bindParam(':ticket', $_GET['subUpdate'], PDO::PARAM_INT);
-	$stmt->bindParam(':state', $_GET['status'], PDO::PARAM_INT);
+	$stmt->bindParam(':problem', $_POST['ptype'], PDO::PARAM_INT);
+	$stmt->bindParam(':description', $_POST['userDesc'], PDO::PARAM_STR);
+	$stmt->bindParam(':ticket', $_POST['subUpdate'], PDO::PARAM_INT);
+	$stmt->bindParam(':state', $_POST['status'], PDO::PARAM_INT);
 
 	if ($stmt->execute() === TRUE){
 	echo("<div class='well'>
-			<div class='alert alert-success'><strong>Ticket #{$_GET['subUpdate']} updated!<strong></div>
+			<div class='alert alert-success'><strong>Ticket #{$_POST['subUpdate']} updated!<strong></div>
 		</div>");
 	} else {
 	echo("<div class='well'>
-			<div class='alert alert-danger'><strong>Ticket #{$_GET['subUpdate']} failed to update, please try again.<strong></div>
+			<div class='alert alert-danger'><strong>Ticket #{$_POST['subUpdate']} failed to update, please try again.<strong></div>
 		</div>");
 	}
 }
