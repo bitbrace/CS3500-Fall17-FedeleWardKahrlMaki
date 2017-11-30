a72f9a7940d46425944ffe92687d52409535eb13
bitbrace <f.allin.kahrl@wmich.edu>
2017-11-29 23:42:27 -0500

    added 404 and htaccess

---
+++ CoreFiles/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>404 File Not Found | Graet Help</title>
+	<link href="../Resources/bootstrap-3.3.7/dist/css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="../Resources/auxStyling.css">
+</head>
+<body>
+    <?php include "includes/header.inc.php"; ?>
+<div class="container">
+<div class="well">
+    <h2>404</h2>
+
+    <p>That isn't here.</p>
+
+</div>
+    </div>
+    <?php include "includes/footer.inc.php"; ?>
+</body>
